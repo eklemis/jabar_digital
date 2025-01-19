f1f034abc09eec4ee4cc675c0f804cb094152381
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2025-01-19 22:03:32 +0800

    used token age from env variables

--- auth_node/src/Infrastructures/security/JwtTokenManager.js
+++ auth_node/src/Infrastructures/security/JwtTokenManager.js
@@ -13,7 +13,7 @@ class JwtTokenManager extends AuthenticationTokenManager {
 
   async createAccessToken(payload) {
     return this._jwt.sign(payload, process.env.ACCESS_TOKEN_KEY, {
-      expiresIn: "1h",
+      expiresIn: process.env.ACCCESS_TOKEN_AGE || "1h",
     });
   }
 
